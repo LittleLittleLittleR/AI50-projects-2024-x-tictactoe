0af92441bb9227ff21439a81c2639e0aba34a483
Little R <131443084+LittleLittleLittleR@users.noreply.github.com>
2024-12-05 01:28:15 +0800

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -56,17 +57,12 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
 
-    row = action[0]
-    column = action[1]
-    if not(0 <= row <= 2) or not(0 <= column <= 2):
-        raise Exception("move is out-of-bounds")
-    if board[row][column] != EMPTY:
-        raise Exception("cell is already occupied")
+    if action not in actions(board):
+        raise Exception("Invalid Move")
 
-    newboard = [row[:] for row in board]
+    row, column = action
+    newboard = copy.deepcopy(board)
     newboard[row][column] = player(board)
-    print(board)
-    print(newboard, row, column, player(board))
 
     return newboard
 
@@ -78,7 +74,7 @@ def winner(board):
 
     # Horizontal
     for row in board:
-        if len(set(row)) == 1:
+        if len(set(row)) == 1 and row[0] != EMPTY:
             return row[0]
 
     # Vertical
@@ -86,13 +82,13 @@ def winner(board):
         tempset = set()
         for row in board:
             tempset.add(row[i])
-        if len(tempset) == 1:
+        if len(tempset) == 1 and EMPTY not in tempset:
             return tempset.pop()
 
     # diagonal
-    if board[0][0] == board[1][1] == board[2][2]:
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != EMPTY:
         return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0]:
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != EMPTY:
         return board[0][2]
 
     return None
@@ -127,14 +123,9 @@ def minimax(board):
     def max_value(board):
         if terminal(board):
             return utility(board)
-        if turn == X:
-            v = -100
-            for action in actions(board):
-                v = max(v, min_value(result(board, action)))
-        else:
-            v = -100
-            for action in actions(board):
-                v = max(v, min_value(result(board, action)))
+        v = -100
+        for action in actions(board):
+            v = max(v, min_value(result(board, action)))
         return v
 
     def min_value(board):
@@ -142,30 +133,25 @@ def minimax(board):
             return utility(board)
         v = 100
         for action in actions(board):
-            v = max(v, max_value(result(board, action)))
-            return v
+            v = min(v, max_value(result(board, action)))
+        return v
 
     turn = player(board)
-    value_lst = []
+    lst = []
     output_act = None
-
     if turn == X:
         output_val = -100
         for action in actions(board):
-            value = max_value(result(board, action))
+            value = min_value(result(board, action))
             if value > output_val:
                 output_val = value
                 output_act = action
     else:
         output_val = 100
         for action in actions(board):
-            value = min_value(result(board, action))
+            value = max_value(result(board, action))
             if value < output_val:
                 output_val = value
                 output_act = action
-
+                
     return output_act
-
-
-
-
